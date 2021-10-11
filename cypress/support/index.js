f6b61f39aa161441e6b22cfc196c8a1b9f5744f7
lalopez13 <lorojas.laure@gmail.com>
2021-10-11 01:25:11 -0500

    add before hook test

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,20 +1,46 @@
 // ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
-import './commands'
-
+import './commands';
+import HomePage from '../pages/homePage';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+beforeEach(() => {
+
+    cy.visit("/");
+    // New instance of home 
+    const home = new HomePage();
+    // Configure to get the correct currency and language
+    cy.configLangAndCurrency();
+
+    // Select destination city
+    home.getDestinationField().type('Cartagena')
+    // wait until the options load
+    cy.wait(5000);
+    home.getListOption().should('have.length', 5).each(($el, index, $list) => {
+        // condition matching check
+        if ($el.text().includes("Cartagena de IndiasBol")) {
+            // click() on that option for selection
+            $el.click();
+        }
+    })
+
+    // assertion to check if correct option is selected
+    home.getDestinationField().should("have.value", "Cartagena de Indias, Bolívar, Colombia");
+
+// Select check- in and checkout date
+    cy.selectDate(90, 110)
+
+    // Select guest option
+    home.getGuestOption().click();
+    home.getGuestContent().should('be.visible');
+    home.getAddBtn().click();
+    cy.wait(3000);
+    cy.get('.xp__guests__count > span:nth-child(1)').should("have.text", '3 adultos');
+    home.getSubmitBtn().click();
+
+})
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+  // failing the test
+    return false
+    })
